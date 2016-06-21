f4a40c48ac12bda9e916d88e475a2b0a8cfeac48
siddharthsingh <siddharth7.singh@gmail.com>
2016-06-21 12:48:17 +0530

    login and signup working with api and error checking

--- app/src/main/java/siddharth/com/chakshu/LoginActivity.java
+++ app/src/main/java/siddharth/com/chakshu/LoginActivity.java
@@ -5,22 +5,27 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
-import java.io.ByteArrayOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Created by siddharthsingh on 12/06/16.
@@ -29,6 +34,8 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     private TextView mSignupBtn;
     private Button mLoginBtn;
     private EditText mPhoneView, mPasswordView;
+    private String result = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,10 +52,25 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         if(v.getId()==R.id.btn_login)
         {
-//            Intent i = new Intent(getApplicationContext(), MainActivity.class);
-//            startActivity(i);
-//            finish();
-            new RequestTask().execute("http://192.168.1.35:8001/loguser/?phone="+mPhoneView.getText()+"&password="+mPasswordView.getText());
+            if(mPhoneView.getText().toString().length()==10 && mPasswordView.getText().toString().length()>=5)
+                new RequestTask().execute("http://192.168.56.74:8001/loguser/");
+            else if(mPhoneView.getText().toString().length()!=10)
+            {
+                new AlertDialog.Builder(LoginActivity.this)
+                        .setTitle("Error")
+                        .setMessage("Enter a valid phone number")
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+            else if(mPasswordView.getText().toString().length()<=5)
+            {
+                new AlertDialog.Builder(LoginActivity.this)
+                        .setTitle("Error")
+                        .setMessage("Minimum password length is 5")
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+
+            }
         }
         else if(v.getId()==R.id.link_signup)
         {
@@ -56,6 +78,28 @@ public class LoginActivity extends Activity implements View.OnClickListener {
             startActivity(i);
             finish();
         }
+    }
+    public String POST(String url) {
+        InputStream is = null;
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("username", mPhoneView.getText().toString()));
+        nameValuePairs.add(new BasicNameValuePair("password", mPasswordView.getText().toString()));
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(url);
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+            if(is != null)
+                result = convertInputStreamToString(is);
+            else
+                result = "Did not work!";
+        } catch (Exception e) {
+            Log.e("HTTP", "Error in http connection " + e.toString());
+        }
+        return result;
+
     }
     class RequestTask extends AsyncTask<String, String, String> {
 
@@ -67,31 +111,10 @@ public class LoginActivity extends Activity implements View.OnClickListener {
             dialog.show();
         }
         @Override
-        protected String doInBackground(String... uri) {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpResponse response;
-            String responseString = "lolz";
-            Log.d("url", uri[0]);
-            try {
-                response = httpclient.execute(new HttpGet(uri[0]));
-                StatusLine statusLine = response.getStatusLine();
-                Log.d("serversentdata", statusLine.toString());
-                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-                    ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    response.getEntity().writeTo(out);
-                    responseString = out.toString();
-                    out.close();
-                } else{
-                    //Closes the connection.
-                    response.getEntity().getContent().close();
-                    throw new IOException(statusLine.getReasonPhrase());
-                }
-            } catch (ClientProtocolException e) {
-                //TODO Handle problems..
-            } catch (IOException e) {
-                //TODO Handle problems..
-            }
-            return responseString;
+        protected String doInBackground(String... uri)
+        {
+
+            return POST(uri[0]);
         }
 
         @Override
@@ -101,5 +124,16 @@ public class LoginActivity extends Activity implements View.OnClickListener {
             //Do anything with response..
             Log.d("serever data", result);
         }
+    }
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
     }
 }
--- app/src/main/java/siddharth/com/chakshu/SignUpActivity.java
+++ app/src/main/java/siddharth/com/chakshu/SignUpActivity.java
@@ -1,15 +1,163 @@
 package siddharth.com.chakshu;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Created by siddharthsingh on 12/06/16.
  */
-public class SignUpActivity extends Activity {
+public class SignUpActivity extends Activity implements View.OnClickListener {
+
+    private Button mSignupBtn;
+    private EditText mPhoneView, mPasswordView, mEmailView, mNameview, mConfirmPasswordView;
+    private String result = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        mSignupBtn = (Button)findViewById(R.id.btn_signup);
+        mPhoneView = (EditText)findViewById(R.id.input_phone);
+        mPasswordView = (EditText)findViewById(R.id.input_password);
+        mConfirmPasswordView = (EditText)findViewById(R.id.input_confpassword);
+        mEmailView = (EditText)findViewById(R.id.input_email);
+        mNameview = (EditText)findViewById(R.id.input_name);
+        mSignupBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.btn_signup)
+        {
+            if(mPhoneView.getText().toString().length()!=10)
+            {
+                    new AlertDialog.Builder(SignUpActivity.this)
+                    .setTitle("Error")
+                    .setMessage("Enter a valid phone number")
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+            }
+            else if(mPasswordView.getText().toString().length()<5)
+            {
+                    new AlertDialog.Builder(SignUpActivity.this)
+                    .setTitle("Error")
+                    .setMessage("Minimum password length is 5")
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+
+            }
+            else if(!mPasswordView.getText().toString().equals(mConfirmPasswordView.getText().toString()))
+            {
+                    new AlertDialog.Builder(SignUpActivity.this)
+                    .setTitle("Error")
+                    .setMessage("Password do not match")
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+                    Log.d("pas", mPasswordView.getText().toString());
+                    Log.d("pas2", mConfirmPasswordView.getText().toString());
+
+            }
+            else if(mNameview.getText().toString().length()==0)
+            {
+                new AlertDialog.Builder(SignUpActivity.this)
+                        .setTitle("Error")
+                        .setMessage("Please enter name")
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+
+            }
+            else if(mEmailView.getText().toString().length()==0)
+            {
+                new AlertDialog.Builder(SignUpActivity.this)
+                        .setTitle("Error")
+                        .setMessage("Please enter email")
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+            else
+            {
+                new RequestTask().execute("http://192.168.56.74:8001/signupuser/");
+            }
+        }
+    }
+    public String POST(String url) {
+        InputStream is = null;
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("username", mPhoneView.getText().toString()));
+        nameValuePairs.add(new BasicNameValuePair("password", mPasswordView.getText().toString()));
+        nameValuePairs.add(new BasicNameValuePair("email",mEmailView.getText().toString()));
+        nameValuePairs.add(new BasicNameValuePair("first_name",mNameview.getText().toString()));
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(url);
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+            if(is != null)
+                result = convertInputStreamToString(is);
+            else
+                result = "Did not work!";
+        } catch (Exception e) {
+            Log.e("HTTP", "Error in http connection " + e.toString());
+        }
+        return result;
+
+    }
+    class RequestTask extends AsyncTask<String, String, String> {
+
+        ProgressDialog dialog = new ProgressDialog(SignUpActivity.this);
+        @Override
+        protected void onPreExecute() {
+            // Show Progress dialog
+            dialog.setMessage("Checking Server..");
+            dialog.show();
+        }
+        @Override
+        protected String doInBackground(String... uri)
+        {
+
+            return POST(uri[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            dialog.dismiss();
+            super.onPostExecute(result);
+            //Do anything with response..
+            Log.d("serever data", result);
+        }
+    }
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
     }
 }
